a04f6fd9a52cdaf3ff3132cfef850f2291f041e7
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-14 23:08:46 +0000

    [Feature]: create file 1-hbnb.js that do the dictionary of the checkbox and create input tag for list of amenities in file 1-hbnb.html

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,8 +2,28 @@
 
 */
 $(document).ready(function () {
-	let algo = $('input:checked').length;
-	console.log(algo);
-
+  let dict = {};
+  var countChecked = function () {
+    if($( ".popover li input" ).prop("checked"), true) {
+      let id = $(this).attr("data-id");
+      let name = $(this).attr("data-name");
+      if (dict.hasOwnProperty(id)) {
+        delete dict[id];
+      } else {
+        dict[id] = name;
+      }
+      if (dict[id] == undefined) {
+        delete dict[id];
+      }
+      console.log(dict);
+      let list_n = []
+      $.each(dict, function (key, val) {
+        list_n.push(val)
+      });
+      $('.amenities h4').text(list_n);
+    }
+  };
+  countChecked();
+  $( "input[type=checkbox]" ).on( "click", countChecked );
 
 });
