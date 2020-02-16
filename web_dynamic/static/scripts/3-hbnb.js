64e4e0d7e674d3472fd016a847a533da406317e3
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-16 14:40:13 -0500

    [Feature]: Implement function that show the content of places for a post method

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -25,28 +25,53 @@ $(document).ready(function () {
   countChecked();
   $( "input[type=checkbox]" ).on( "click", countChecked );
 
-  $.get('http://localhost:5001/api/v1/status/', function(data, textStatus) {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus) {
     if(textStatus === 'success') {
       $('#api_status').toggleClass('enable available');
     }
   });
 
   $.ajax({
-    url: "http://localhost:5001/api/v1/places_search/",
+    url: "http://0.0.0.0:5001/api/v1/places_search/",
     type: "POST",
     contentType: "application/json",
     data: "{}",
-    success: function( data, textStatus, jQxhr ){
-      $.each(data, function (index, val) {
-        $('.title h2').text(val.name)
-        $('.price_by_night').text(val.price_by_night)
-        $('.max_guest').append(val.max_guest + " Guests")
-        $('.number_rooms').append(val.number_rooms + " Bedrooms")
-        $('.number_bathrooms').append(val.number_bathrooms + " Bathroom")
-        console.log(val);
-
+    success: function( data, textStatus){
+      $.each(data, function(key, val) {
+        $(".places").append(`
+              <article>
+                <div class="title">
+                  <h2>${val.name}</h2>
+                  <div class="price_by_night">
+                    ${val.price_by_night}
+                  </div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">
+                    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${val.max_guest} Guests
+                  </div>
+                  <div class="number_rooms">
+                    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${val.number_rooms} Bedrooms
+                  </div>
+                  <div class="number_bathrooms">
+                    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+                    <br />
+                    ${val.number_bathrooms} Bathrooms
+                  </div>
+                </div>
+                <div class="user">
+                  <strong>Owner: PENDIENT</strong>
+                </div>
+                </div class="description">
+                  ${val.description}
+                </div>
+              </article>`);
       });
-
+     
     },
     error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
