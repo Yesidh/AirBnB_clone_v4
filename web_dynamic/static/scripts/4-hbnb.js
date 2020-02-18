748b58fb9fb2015e341a343ee44512edbfea8a37
Yesidh <944@holbertonschool.com>
2020-02-18 00:30:58 -0500

    [Fix]: adding the html code to show the request elements

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,73 +1,135 @@
 /*
-
+========================================================
+Script to show the filter using amenities
+========================================================
 */
 $(document).ready(function () {
-  let dict = {};
-  let countChecked = function () {
-    if($( ".popover li input" ).prop("checked"), true) {
-      let id = $(this).attr("data-id");
-      let name = $(this).attr("data-name");
+  // function to show the amenities checked by the user
+  const dict = {};
+  const countChecked = function () {
+    if ($('.popover li input').prop('checked', true)) {
+      const id = $(this).attr('data-id');
+      const name = $(this).attr('data-name');
       if (dict.hasOwnProperty(id)) {
         delete dict[id];
       } else {
         dict[id] = name;
       }
-      if (dict[id] == undefined) {
+      if (dict[id] === undefined) {
         delete dict[id];
       }
-      let list_n = []
+      const list = [];
       $.each(dict, function (key, val) {
-        list_n.push(val)
+        list.push(val);
       });
-      $('.amenities h4').text(list_n);
+      $('.amenities h4').text(list);
     }
-      return dict;
+    return dict;
   };
-  $( "input[type=checkbox]" ).on( "click", countChecked );
 
-  $.get('http://localhost:5001/api/v1/status/', function(data, textStatus) {
-    if(textStatus === 'success') {
+  // calling the on click each time the user check one
+  $('input[type=checkbox]').on('click', countChecked);
+  // checking the API Status
+  $.get('http://localhost:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
       $('#api_status').toggleClass('enable available');
     }
   });
 
+  // making a POST to show initial places
   $.ajax({
-    url: "http://localhost:5001/api/v1/places_search/",
-    type: "POST",
-    contentType: "application/json",
-    data: "{}",
-    success: function( data, textStatus, jQxhr ){
-      $.each(data, function (index, val) {
-        $('.title h2').text(val.name)
-        $('.price_by_night').text(val.price_by_night)
-        $('.max_guest').append(val.max_guest + " Guests")
-        $('.number_rooms').append(val.number_rooms + " Bedrooms")
-        $('.number_bathrooms').append(val.number_bathrooms + " Bathroom")
-        console.log(val);
-
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data, textStatus) {
+      $.each(data, function (key, val) {
+        $('.places').append(`
+			    <article>
+			    <div class="title">
+			    <h2>${val.name}</h2>
+			    <div class="price_by_night">
+			    ${val.price_by_night}
+			    </div>
+			    </div>
+			    <div class="information">
+			    <div class="max_guest">
+			    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+			    <br />
+			    ${val.max_guest} Guests
+			    </div>
+			    <div class="number_rooms">
+			    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+			    <br />
+			    ${val.number_rooms} Bedrooms
+			    </div>
+			    <div class="number_bathrooms">
+			    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+			    <br />
+			    ${val.number_bathrooms} Bathrooms
+			    </div>
+			    </div>
+			    <div class="user">
+			    <strong>Owner: PENDIENT</strong>
+			    </div>
+			    </div class="description">
+			    ${val.description}
+			    </div>
+			    </article>`);
       });
-
     },
-    error: function( jqXhr, textStatus, errorThrown ){
-        console.log( errorThrown );
+    error: function (jqXhr, textStatus, errorThrown) {
+	  console.log(errorThrown);
     }
   });
 
-    $('button').click(function () {
-      let alguito = countChecked();
-      let algo_mas = JSON.stringify(alguito);
-      console.log(alguito);
-	console.log(algo_mas);
-	$.ajax({
-	    url: "http://localhost:5001/api/v1/places_search/",
-	    type: "POST",
-	    contentType: "application/json",
-	    data: algo_mas,
-	    success: function( data, textStatus, jQxhr ){
-		console.log('en el post', data);
-	    },
-	});
+  // searchin the amenities checked by the user
+  $('button').click(function () {
+    const dictToJson = JSON.stringify(countChecked());
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: dictToJson,
+      success: function (data, textStatus, jQxhr) {
+        $.each(data, function (key, val) {
+          $('.places').append(`
+			      <article>
+			      <div class="title">
+			      <h2>${val.name}</h2>
+			      <div class="price_by_night">
+			      ${val.price_by_night}
+			      </div>
+			      </div>
+			      <div class="information">
+			      <div class="max_guest">
+			      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+			      <br />
+			      ${val.max_guest} Guests
+			      </div>
+			      <div class="number_rooms">
+			      <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+			      <br />
+			      ${val.number_rooms} Bedrooms
+			      </div>
+			      <div class="number_bathrooms">
+			      <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+			      <br />
+			      ${val.number_bathrooms} Bathrooms
+			      </div>
+			      </div>
+			      <div class="user">
+			      <strong>Owner: PENDIENT</strong>
+			      </div>
+			      </div class="description">
+			      ${val.description}
+			      </div>
+			      </article>`);
+        });
+      },
+      error: function (jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
     });
-
-
+  });
 });
